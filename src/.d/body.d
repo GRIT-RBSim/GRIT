c5e0db7e06099a966f03deff1766087c1b036313
Renyi Chen <renyichen@google.com>
2023-06-28 18:41:39 -0700

    Remove some generated files.

--- src/.d/body.d
+++
@@ -1,9 +0,0 @@
-body.o: body.cpp body.hpp math_func.hpp matrix.hpp ../include/json.hpp
-
-body.hpp:
-
-math_func.hpp:
-
-matrix.hpp:
-
-../include/json.hpp:
--- src/.d/math_func.d
+++
@@ -1,7 +0,0 @@
-math_func.o: math_func.cpp math_func.hpp matrix.hpp ../include/json.hpp
-
-math_func.hpp:
-
-matrix.hpp:
-
-../include/json.hpp:
